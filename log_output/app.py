edf6f5a8b7e50f984ef35f858d90eb1fb387ccf2
Mhonkasalo <manehonka@gmail.com>
2025-11-17 15:55:08 +0200

    Exercise 1.7

--- log_output/app.py
+++ log_output/app.py
@@ -1,11 +1,29 @@
 import time, uuid, datetime
+from flask import Flask
+import os
+import threading
 
 value = str(uuid.uuid4())
 
-while True:
-	aika = datetime.datetime.utcnow().isoformat() + "Z"
-	string = aika + ": " + value
-	print(string, flush=True)
-	time.sleep(5)
+app = Flask(__name__)
+port = int(os.getenv("PORT",3000))
+
+def print_logs():
+	while True:
+		aika = datetime.datetime.utcnow().isoformat() + "Z"
+		string = aika + ": " + value
+		print(string, flush=True)
+		time.sleep(5)
 
 
+@app.get("/status")
+def status():
+    aika = datetime.datetime.utcnow().isoformat() + "Z"
+    return f"{aika}: {value}"
+      
+if __name__ == "__main__":
+    threading.Thread(target=print_logs, daemon=True).start()
+    app.run(host="0.0.0.0", port=port)
+
+	
+	
