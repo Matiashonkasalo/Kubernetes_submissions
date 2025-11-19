b1733a832c63f8bde51451c2725814bba7cd95be
Mhonkasalo <manehonka@gmail.com>
2025-11-19 12:07:07 +0200

    Exercise 1.11

--- log_output/reader/reader.py
+++ log_output/reader/reader.py
@@ -1,10 +1,10 @@
 from flask import Flask
 import os
 
-
 app = Flask(__name__)
 port = int(os.getenv("PORT",3000))
 LOG_PATH = "/shared/log.txt"
+LOG_PATH_PONG = "/shared/pongcount.txt"
 
 @app.get("/")
 def file_read():
@@ -14,18 +14,28 @@ def file_read():
         return f"<pre>{lines}</pre>"
     except FileNotFoundError:
         return "File not found :("
-
+    
 @app.get("/status")
 def status():
     try:
         with open(LOG_PATH, "r") as f:
             lines = f.readlines()
         if lines:
-            return lines[-1]
-        return "no logs yet"
+            last_log = lines[-1].strip()
+        else:
+            last_log = "No logs yet"
     except FileNotFoundError:
-        return "log file not found"
+        return "Log file not found"
+    try:
+        with open(LOG_PATH_PONG,"r") as f:
+            pong_count = f.read().strip()
+        if pong_count == "":
+            pong_count = "0"
+    except:
+        pong_count = "0"
 
+    return f"{last_log} \n Ping / Pongs: {pong_count}"
+        
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
--- pingpong/app.py
+++ pingpong/app.py
@@ -1,16 +1,31 @@
 from flask import Flask
 import os
 
-
 app = Flask(__name__)
+
+LOG_PATH = "/shared/pongcount.txt"
 
 port = int(os.getenv("PORT",3001))
 counter = 0
 
+def read_counter():
+    if not os.path.exists(LOG_PATH):
+        return 0
+    with open(LOG_PATH, "r") as f:
+        try:
+            return int(f.read().strip())
+        except:
+            return 0
+
+def write_counter(value):
+    with open(LOG_PATH, "w") as f:
+        f.write(str(value))
+        
 @app.get("/pingpong")
 def pong():
-    global counter
-    counter = counter + 1
+    counter = read_counter()
+    counter += 1
+    write_counter(counter)
     return f"pong {counter}"
 
 if __name__ == "__main__":
