6ef413daf38593f41dc3c71ca30bc3fdcdcc36ac
Mhonkasalo <manehonka@gmail.com>
2025-11-20 13:54:27 +0200

    Exercise 2.2

---
+++ Project/Backend_todo/todo_back.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import os
+
+port = int(os.getenv("PORT", 3002))
+app = Flask(__name__)
+
+print(f"Backend server running on {port}")
+##apista klick -> tänne appedn list
+Frontend_URL = "http://project-svc:1234"
+
+list_of_todos = []
+
+##receiving new a new todo and updating the list
+@app.post("/todos")
+def getting_todos():
+    data = request.get_json() 
+    if not data or "content" not in data:
+        return "Missing todo content", 400
+    content = data["content"]
+    if len(content)>140:
+        return "Todo is over 140 characters, try again"
+    
+    list_of_todos.append(content)
+    return "OK", 200
+
+#tranfering the todos back to front
+@app.get("/todos")
+def transfer_todos():
+    return jsonify(list_of_todos)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
